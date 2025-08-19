895857558355a22a068de4c03cdcaaea8be9032a
youssefshoukry1 <youssefshoukry1@hotmail.com>
2025-08-20 02:10:09 +0300

    update project

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -57,7 +57,7 @@ export default function About() {
         className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-6"
         variants={containerVariants}
         initial="hidden"
-        animate="visible"
+        whileInView="visible"
       >
         {cards.map(({ icon, title, content }) => (
           <Motion.div
