6f765a10a3d8d93bea88c3f67942b053aa238970
youssefshoukry1 <youssefshoukry1@hotmail.com>
2025-09-01 09:22:18 +0300

    update project

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from "react";
 import { motion as Motion } from "framer-motion";
 import { FaGraduationCap, FaClipboardList, FaUsers, FaBriefcase, FaStar } from "react-icons/fa";
 
